b9260e11afef1b581103da0f4d56a7459d0317b7
Alan Tavares <alan.tavares@superlogica.com>
2022-03-04 06:00:52 -0300

    removing vpc link

--- infra/ecs/vpc-link.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_api_gateway_vpc_link" "nlb_link" {
-  name        = "vpclink-${var.cluster_name}"
-  description = "Link between API Gateway and application network"
-  target_arns = [aws_lb.app_nlb.arn]
-  depends_on = [aws_lb.app_nlb]
-}
