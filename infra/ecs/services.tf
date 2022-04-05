be48835bb2d80be2f8aae41daea04477c4e77023
Alan Tavares <alan.tavares@superlogica.com>
2022-04-05 06:11:43 -0300

    removing migration tasks

--- infra/ecs/services.tf
+++ infra/ecs/services.tf
@@ -32,25 +32,3 @@ resource "aws_ecs_service" "web-api" {
 
   # depends_on = [aws_alb_target_group.api_target_group]
 }
-
-resource "aws_ecs_service" "migration" {
-  name            = var.service_migration_name
-  task_definition = aws_ecs_task_definition.migration.family
-  cluster         = aws_ecs_cluster.cluster.id
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  depends_on = [aws_iam_role_policy.ecs_service_role_policy]
-
-  network_configuration {
-    security_groups = [
-      aws_security_group.app_sg.id,
-      aws_security_group.alb_sg.id
-    ]
-    subnets = [
-      var.subnet1,
-      var.subnet2
-    ]
-    assign_public_ip = true
-  }
-}
