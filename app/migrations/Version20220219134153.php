68566b2a1a4173e8b0e04ec0b6402f1c792d6b6e
Alan Tavares <alan.tavares@superlogica.com>
2022-04-05 21:56:15 -0300

    try to correct migrate php 8

--- app/migrations/Version20220219134153.php
+++ app/migrations/Version20220219134153.php
@@ -28,4 +28,9 @@ final class Version20220219134153 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE client CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
 }
